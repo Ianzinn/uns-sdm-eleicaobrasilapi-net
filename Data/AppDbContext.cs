92dc05a6443582bf6e623cdb323451c85efac8f1
Ian Santana <ian.santana@onfly.com.br>
2026-03-30 20:27:40 -0300

    Initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using EleicaoBrasilApi.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EleicaoBrasilApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Candidato> Candidatos { get; set; }
+    }
+}
